4fbaf239bf933465aaa7e9c12c284fcb3c06e80b
dgellow <samuel.elborai@gmail.com>
2017-01-05 12:23:53 +0100

    in C-instruction dest bits are after conditions

--- src/Compiler.cpp
+++ src/Compiler.cpp
@@ -15,15 +15,6 @@ string Compiler::compile(Instructions instructions) {
       // opcode
       output += "111";
 
-      // dest
-      if (instruction.dest.value.find_first_not_of(Grammar::legalDest) == string::npos) {
-        output += instruction.dest.value.find(Grammar::legalDest[0]) != string::npos ? "1" : "0";
-        output += instruction.dest.value.find(Grammar::legalDest[1]) != string::npos ? "1" : "0";
-        output += instruction.dest.value.find(Grammar::legalDest[2]) != string::npos ? "1" : "0";
-      } else {
-        throw invalid_argument("Compiler error: Invalid destination in C instruction");
-      }
-
       // cond
       string cond = "";
       for (auto t : instruction.cond) {
@@ -43,7 +34,16 @@ string Compiler::compile(Instructions instructions) {
         auto bits = Grammar::legalCond.at(cond);
         output += bitset<7>(bits).to_string();
       } catch(const out_of_range e) {
-        throw invalid_argument("Compiler error: Invalid condition in C instruction cond=" + cond);
+        throw invalid_argument("Compiler error: Invalid condition in C instruction");
+      }
+
+      // dest
+      if (instruction.dest.value.find_first_not_of(Grammar::legalDest) == string::npos) {
+        output += instruction.dest.value.find(Grammar::legalDest[0]) != string::npos ? "1" : "0";
+        output += instruction.dest.value.find(Grammar::legalDest[1]) != string::npos ? "1" : "0";
+        output += instruction.dest.value.find(Grammar::legalDest[2]) != string::npos ? "1" : "0";
+      } else {
+        throw invalid_argument("Compiler error: Invalid destination in C instruction");
       }
 
       // jump
